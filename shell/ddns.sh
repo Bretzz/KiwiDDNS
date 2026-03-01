598c17d854d46d6362e9052a6e9789039229b03f
Bretzz <tommaso.piana.agostinetti@gmail.com>
2026-03-01 03:32:41 +0100

    Removed logs of ips

--- shell/ddns.sh
+++ shell/ddns.sh
@@ -1,30 +1,34 @@
 #!/bin/sh
-set -e  # Exit immediately if any command exits with a non-zero status
 
-if [ -f .env ]; then
-    set -a            # Automatically export all variables
-    source .env       # Read the file
-    set +a            # Disable automatic export
-fi
+# Exit immediately if any command exits with a non-zero status
+set -e
+
+ # This moves the script's context to its own folder
+cd "$(dirname "$0")"
+
+# This loads the env variables
+source .env
 
 echo 'Updating DNS to redirect to current IP ...'
-[ -z "$ACCOUNT_ID" ] && echo "Error: Could not get Account ID from .env" && exit 1
-[ -z "$DOMAIN_NAME" ] && echo "Error: Could not get Domain Name from .env" && exit 1
-[ -z "$CLOUDFLARE_API_TOKEN" ] && echo "Error: Could not get Cloudflare API Token from .env" && exit 1
 
-echo Account: $ACCOUNT_ID Domain: $DOMAIN_NAME Token: $CLOUDFLARE_API_TOKEN
+[ -z "$ACCOUNT_ID" ] && echo "Error: Could not get Account ID from .env" && exit 1
+echo 'Account OK'
+[ -z "$DOMAIN_NAME" ] && echo "Error: Could not get Domain Name from .env" && exit 1
+echo 'Domain OK'
+[ -z "$CLOUDFLARE_API_TOKEN" ] && echo "Error: Could not get Cloudflare API Token from .env" && exit 1
+echo 'Token OK'
 
 RECORD_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records" \
 	-H "Authorization: Bearer $CLOUDFLARE_API_TOKEN" \
 	-H "Content-Type: application/json" | jq -r '.result[0].id')
 
 [ -z "$RECORD_ID" ] && echo "Error: Could not get Record ID" && exit 1
-echo "Found RECORD_ID $RECORD_ID"
+echo "Record ID OK"
 
 MY_IP=$(curl -s https://ifconfig.me)
 
 [ -z "$MY_IP" ] && echo "Error: Could not get IP" && exit 1
-echo "MY_IP $MY_IP"
+echo "IP OK"
 
 curl -X PATCH "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$RECORD_ID" \
 	-H "Authorization: Bearer $CLOUDFLARE_API_TOKEN" \
@@ -35,6 +39,4 @@ curl -X PATCH "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$
 		\"proxied\": false
 	}"
 
-
-
-# 37.182.153.134
+echo 'Done'
